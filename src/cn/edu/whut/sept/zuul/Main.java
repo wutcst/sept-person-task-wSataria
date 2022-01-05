9f2239cee0d60edc13009be954d1a70a2c654986
wSataria <86592548+wSataria@users.noreply.github.com>
2022-01-05 16:31:37 +0800

    Update Main.java

--- src/cn/edu/whut/sept/zuul/Main.java
+++ src/cn/edu/whut/sept/zuul/Main.java
@@ -1,11 +1,13 @@
+/**
+ * 程序入口，创建并开始游戏。
+ */
 package cn.edu.whut.sept.zuul;
 
-/**
- * test
- *
- */
 public class Main {
 
+	/**
+	 * 创建游戏，调用play方法.
+	 */
     public static void main(String[] args) {
         Game game = new Game();
         game.play();
